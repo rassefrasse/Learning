ac69cae36a1a76a3626e8bf223e6c600c3d180f5
RasmusE <rasmusekblom@gmail.com>
2023-08-18 15:56:11 +0200

    first commit

---
+++ iwascrazyonce.py
@@ -0,0 +1,8 @@
+while True:
+    print("Crazy? I was crazy once...")
+    print("They put me in a room...")
+    print("A rubber room...")
+    print("A rubber room with rats...")
+    print("They put me in a room with rubber rats.")
+    print("Rubber rats? I hate rubber rats.")
+    print("They make me crazy.")
