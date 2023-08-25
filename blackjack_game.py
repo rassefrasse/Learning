afc9eb5dd261b48200bd81e5e1e17fc656dc1b3b
RasmusE <rasmusekblom@gmail.com>
2023-08-25 14:42:22 +0200

    small commit today im sick

--- blackjack_game.py
+++ blackjack_game.py
@@ -1,43 +1,36 @@
 import random
 
-class Hand:
-
-    hand_c = []
+class Card:
 
 
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
 
-class Dealer:
-    two_c = 2
-    three_c = 3
-    four_c = 4
-    five_c = 5
-    six_c = 6
-    seven_c = 7
-    eight_c = 8 
-    nine_c = 9 
-    face_c = 10
-    ace_c = 11
+    def show(self):
+        print("{} of {}".format(self.rank, self.suit))
 
-    def __init__(self, ):
+
+class Dealer:
+
+
+    def __init__(self):
         pass
 
 
 class Deck:
 
-    deck_c = []
+    def __init__(self):
+        self.deck = []
 
-    def __init__(self, card_deck, draw_card):
-        self.card_deck = card_deck
-        self.draw_card = draw_card
+    def build(self):
+        for s in ("Hearts", "Spades", "Clubs", "Diamonds"):
+            for r in range(2, 12):
+                self.deck.append(Card(r, s))
 
-    def deck():
-        pass
-
-    def draw(self, draw_card):
-        pass
+    def show(self):
+        for card in self.deck:
+            card.show()
         
 
 class Game:
@@ -47,12 +40,6 @@ class Game:
     def rules(self):
         pass
 
-
-
-hit_or_stand = input("H/S: ")
-
-if hit_or_stand == "H":
-  #  Deck.deck_c.pop(random.randrange)
-    pass
-elif hit_or_stand == "S":
-    pass
+cards = Deck()
+cards.build()
+cards.show()
