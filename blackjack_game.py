ae78f426d80c2d279700f56a0085874db5dd933a
RasmusE <rasmusekblom@gmail.com>
2023-08-25 14:46:37 +0200

    first commit of the day, i'm sick so chilling

--- blackjack_game.py
+++ blackjack_game.py
@@ -1,14 +1,14 @@
-import random
+import random #Will be using soon
 
 class Card:
 
 
-    def __init__(self, rank, suit):
+    def __init__(self, rank, suit): #defining the rank and the suit of the cards
         self.rank = rank
         self.suit = suit
 
     def show(self):
-        print("{} of {}".format(self.rank, self.suit))
+        print("{} of {}".format(self.rank, self.suit)) #prints the card i.e. "2 of Spades"
 
 
 class Dealer:
@@ -20,26 +20,26 @@ class Dealer:
 
 class Deck:
 
-    def __init__(self):
-        self.deck = []
+    def __init__(self): 
+        self.deck = [] #The deck where the cards are stored
 
     def build(self):
-        for s in ("Hearts", "Spades", "Clubs", "Diamonds"):
+        for s in ("Hearts", "Spades", "Clubs", "Diamonds"): #The method that builds the deck by running suit and rank in a f loop
             for r in range(2, 12):
-                self.deck.append(Card(r, s))
+                self.deck.append(Card(r, s)) #Sends the value of the cards to the deck above
 
     def show(self):
-        for card in self.deck:
+        for card in self.deck: #Method to show the card
             card.show()
         
 
 class Game:
-    def __init__(self):
+    def __init__(self): #WIP
         pass
 
     def rules(self):
         pass
 
 cards = Deck()
-cards.build()
-cards.show()
+cards.build() #Run the creation of the cards>>
+cards.show() #Just displaying the cards
